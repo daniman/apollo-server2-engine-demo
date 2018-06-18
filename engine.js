4aac1180cc4ea8259778b9ca95da691cfef2ee1b
Danielle Man <daniman0808@gmail.com>
2018-06-19 00:35:13 +0200

    Cleanup

--- engine.js
+++ engine.js
@@ -2,9 +2,9 @@ const { ApolloEngineLauncher } = require('apollo-engine');
 
 // Define the Engine configuration.
 const launcher = new ApolloEngineLauncher({
-  // Enter your API key from step 2 above. You can also provide this
-  // in the ENGINE_API_KEY environment variable.
-  apiKey: 'service:lafourchette:2NJtgGv2zM43B21qoDqbXQ',
+  // Get your Engine API key by creating a service on
+  // engine.apollographql.com.
+  apiKey: 'YOUR_ENGINE_API_KEY',
   origins: [{
     http: {
       // The URL that the Proxy should use to connect to your
